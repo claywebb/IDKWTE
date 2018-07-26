e676be033a1410496f1b6d4945c1a35280e6ec3d
David Thames <davidct@vt.edu>
2018-07-26 15:23:31 -0700

    Updated requests import

--- yelp.py
+++ yelp.py
@@ -1,4 +1,4 @@
-import requests
+from botocore.vendored import requests
 import json
 try:
     #python2
@@ -7,7 +7,7 @@ except ImportError:
     #python3
     from urllib.parse import urlencode
 
-AUTH_TOKEN = ''
+AUTH_TOKEN = 'isB958Rd1cSOaUSREJBtaJEKzsYvwUkpx0wxf56323V2udIzJoG1TNuB-oLMENDWX5Ve3qOL2iN82p3Qsoa9rgejPI2ypr5jOEu-wRX-fSrjTf2mK3TRiYCJrSJaW3Yx'
 
 def getYelp(location):
     url = 'https://api.yelp.com/v3/businesses/search'
