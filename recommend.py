a5ab79037c273aaeebc0a83c1fc703e586bb1c30
Ishan Timalsina <ishantm@amazon.com>
2018-07-26 15:55:00 -0700

    get next

--- recommend.py
+++ recommend.py
@@ -1,8 +1,8 @@
 from math import log
 import random
 
-max_radius = 1.0;
-selection_multiplier = .2;
+max_radius = 1600;
+prob_selec = max(random.triangular(0,.1,.9),random.triangular(0,.1,.4));
 
 def set_radius(radius):
     global max_radius
@@ -15,12 +15,26 @@ def set_selectionmult(mult):
 def decision(probability):
     return random.random() < probability
 
+def select_num(totRest):
+    global prob_selec
+    for n in range(1,totRest+1):
+        selected = decision(prob_selec)
+        if selected:
+            break
+        prob_selec = prob_selec * .92
+    if (n == totRest):
+        return random.randint(0,totRest)
+    else:
+        return n
+
 def get_goodness(distance, rating):
-    goodness = log(rating) + ((max_radius - distance) / 5)
+    goodness = log(rating) + (((max_radius/1600) - (distance/1600)) / 5)
     return goodness
 
-def get_next_restaurant(restaurant):
-
+def get_next_restaurant(restaurants):
+    totRest = len(restaurants)
+    nextInt = select_num(totRest)
+    return restaurants[nextInt]
 
 def eliminate_below_rating(restaurants, min_rating):
     for restaurant in restaurants:
