fa551862860f95b3354e63cc134ac88e328afc84
Ishan Timalsina <ishantm@amazon.com>
2018-07-26 13:40:45 -0700

    restaurant recommendation functions

---
+++ recommend.py
@@ -0,0 +1,27 @@
+from math import log
+
+max_radius = 1.0;
+selection_multiplier = .2;
+
+def set_radius(radius):
+    global max_radius
+    max_radius = radius
+
+def set_selectionmult(mult):
+    global selection_multiplier
+    selection_multiplier = mult
+
+def get_goodness(distance, rating):
+    goodness = log(rating) + ((max_radius - distance) / 5)
+    return goodness
+
+def get_next_restaurant(restaurant):
+
+
+def eliminate_below_rating(restaurants, min_rating):
+    for restaurant in restaurants:
+        if restaurant["rating"] < min_rating:
+            eliminate_restaurant(restaurant)
+
+def eliminate_restaurant(restaurants, restaurant):
+    restaurants.remove(restaurant)
